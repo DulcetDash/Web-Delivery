d9690acec47f8f6a124777ce65f0ffc5dc54f363
Dominique <dominique@dulcetdash.com>
2023-12-19 21:10:57 +0200

    Update version number in Sidebar component

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -234,7 +234,7 @@ class Sidebar extends React.PureComponent {
                     : "#01101f",
                 }}
               />
-              <span className="menuTextVersionNo">v1.0.043</span>
+              <span className="menuTextVersionNo">v1.0.1</span>
             </Link>
           </MenuItem>
         </Menu>
