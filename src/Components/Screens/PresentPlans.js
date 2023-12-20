b0805732a10f950a8ba64f750a205d30e11d6f6b
Dominique <dominique@dulcetdash.com>
2023-12-20 23:22:25 +0200

    Added credentials

--- src/Components/Screens/PresentPlans.js
+++ src/Components/Screens/PresentPlans.js
@@ -49,7 +49,7 @@ class PresentPlans extends React.PureComponent {
           headers: {
             Authorization: `Bearer ${userData?.company_fp}`,
           },
-          withCredentials: true,
+          withCredentials: false,
         }
       );
 
